f47cb933c8b0b0471ce4454440e6533b3bca634e
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2015-11-21 17:22:53 +0900

    create project analysis

--- optimization/src/hill_climbing.cpp
+++ optimization/src/hill_climbing.cpp
@@ -1,5 +1,6 @@
 #include "hill_climbing.h"
 #include <iostream>
+#include <cmath> //for fabs()
 using namespace std;
 
 Hill_climbing::Hill_climbing(const double& h0, const double& eps, const double& delta)
@@ -14,7 +15,12 @@ void Hill_climbing::compute(){
 }
 
 double Hill_climbing::linear_search(double (*f)(const double&), double(*df)(const double&), const double& x0){
+    double h = h0_;
     double x = x0;
+
+    while(fabs(df(x)) > eps_){
+//        h =
+    }
 
     return x;
 }
