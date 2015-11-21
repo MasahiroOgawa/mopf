17fb06327c849c875a113d8b296ad0a4231c509d
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2015-11-21 10:49:09 +0900

    update Hill_clibming{}: update constructor

--- optimization/src/hill_climbing.cpp
+++ optimization/src/hill_climbing.cpp
@@ -1,7 +1,13 @@
 #include "hill_climbing.h"
 
-Hill_climbing::Hill_climbing()
+Hill_climbing::Hill_climbing(const double& h0, const double& eps, const double& delta)
+    : h0_{h0}, eps_{eps}, delta_{delta}
 {
+
+}
+
+
+void Hill_climbing::compute(){
 
 }
 
--- optimization/src/hill_climbing.h
+++ optimization/src/hill_climbing.h
@@ -2,12 +2,20 @@
 #define HILL_CLIMBING_H
 
 
+/////////////////
+/// \brief The Hill_climbing class
+/// based on "これなら分かる最適化数学　金谷健一" p.81
+/////////////////
 class Hill_climbing
 {
 public:
-    Hill_climbing();
+    Hill_climbing(const double& h0, const double& eps, const double& delta);
+    void compute();
     void linear_search();
-
+private:
+    double h0_;
+    double eps_;
+    double delta_;
 };
 
 #endif // HILL_CLIMBING_H
