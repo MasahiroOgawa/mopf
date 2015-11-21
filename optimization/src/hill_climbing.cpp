8348fef9e4daab6f95703339cb9ebf9f893e9942
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2015-11-21 18:06:50 +0900

    update Hill_climbing{}: implement linear_search()

--- optimization/src/hill_climbing.cpp
+++ optimization/src/hill_climbing.cpp
@@ -22,8 +22,27 @@ double Hill_climbing::linear_search(double (*f)(const double&), double(*df)(cons
     double h = h0_;
     double x = x0;
 
+    //search until |df(x)| ~ 0
     while(fabs(df(x)) > eps_){
-        h *= sgn(df(x));
+        //init x_next
+        h = sgn(df(x)) * fabs(h);
+        double x_next = x + h;
+
+        if(f(x) < f(x_next)){ //in case f(x_next) does not yet reached the neighborhood of maximal
+            while(f(x) < f(x_next)){ //loop until over maximal
+                h *= 2.0;
+                x = x_next;
+                x_next = x + h;
+            }
+            h /= 2.0;
+        }else{ //in case f(x_next) over the neighborhood of maximal
+            while(f(x) >= f(x_next)){ //loop until it finds
+                h /= 2.0;
+                x_next -= h;
+            }
+            x = x_next;
+            h *= 2.0;
+        }
     }
 
     return x;
--- optimization/src/test/hill_climbing.test.cpp
+++ optimization/src/test/hill_climbing.test.cpp
@@ -1,26 +1,28 @@
 #include "../hill_climbing.h"
+#include <iostream>
 
 using namespace std;
 using namespace mo;
 
 
 double f(const double& x){
-    return x*x;
+    return -x*x;
 }
 
 double df(const double& x){
-    return 2*x;
+    return -2*x;
 }
 
 
 int main(){
     double h0 = 1.0;
-    double eps = 1.0e-5;
-    double delta = 1.0e-5;
+    double eps = 1.0e-3;
+    double delta = 1.0e-3;
     Hill_climbing hc(h0,eps,delta);
 
     double x0 = 8.0;
     double x = hc.linear_search(f,df,x0);
+    cout << "local maxima = " << x << endl;
 
 
     return 0;
