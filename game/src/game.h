6cabc10229e3200ac84c13f9e9b20e17e4aa9d8b
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2019-02-11 11:44:45 +0900

    game: remove unwanted contents from game.h

--- game/src/game.h
+++ game/src/game.h
@@ -2,68 +2,9 @@
 #define VISUALIZATION_H
 
 #include "../../image/src/image.h"
-#include <opencv2/imgproc.hpp> //for ColormapTypes, arrowedLine()
 
 namespace mo {
 
-/////////////////////////
-/// \brief show
-/// \param winname
-/// \param img
-/// \param waitms
-/// \return pressed key as int. same with cv::waitKey's return value
-/////////////////////////
-int show(const std::string& winname, const Image& img, const int waitms=0);
-int show(const std::string& winname, const Image_gray& img, const int waitms=0);
-
-
-/////////////////////////
-/// \brief destroy_window
-/// \param winname
-/////////////////////////
-void destroy_window(const std::string& winname);
-
-
-/////////////////////////
-/// \brief line
-/// draw line in img.
-/////////////////////////
-void line(const Point& pt_sta, const Point& pt_end, Image& img, const Color& color = Color{255,255,255});
-
-
-/////////////////////////
-/// \brief arrow
-/// draw arrow in img.
-/// \param pt_sta
-/// \param pt_end
-/// \param img
-/// \param color
-//////////////////////////
-void arrow(const Point& pt_sta, const Point& pt_end, Image& img, const Color& color = Color{255,255,255});
-
-
-//////////////////////////
-/// \brief point
-/// draw point in img.
-//////////////////////////
-void point(const Point& pt, Image& img, const Color& color = Color{255,255,255}, const int radius = 1);
-
-
-//////////////////////////
-/// \brief text
-/// draw text in img.
-//////////////////////////
-void text(const std::string& text, Image& img, const Point& org, const double& scale=1.0, const Color& color = Color{255,255,255});
-
-
-using ColormapTypes = cv::ColormapTypes;
-
-//////////////////////////
-/// \brief colorbar
-/// \param img
-/// \param colormaptype
-//////////////////////////
-void colorbar(Image& img, const ColormapTypes colormaptype);
 
 } //namespace mo
 
