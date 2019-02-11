d9ff87df1cd141f4a531584cd337b139eab5a082
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2019-02-11 11:41:09 +0900

    game: create game

---
+++ game/src/game.cpp
@@ -0,0 +1,11 @@
+#include "game.h"
+#include <opencv2/highgui.hpp>
+using namespace std;
+
+namespace mo {
+
+//-----------------------------------------------------------------
+
+
+
+} //namespace mo
