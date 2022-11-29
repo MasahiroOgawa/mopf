f7e87d628216bd6f6fcf4b273a5792111bb4ee68
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2022-11-30 08:58:15 +0900

    add video directory.

---
+++ video/src/video.cpp
@@ -0,0 +1,6 @@
+#include "video.h"
+using namespace std;
+
+namespace mo {
+
+} //namespace mo
---
+++ video/src/video.h
@@ -0,0 +1,12 @@
+#ifndef VIDEO_H
+#define VIDEO_H
+#include <opencv2/core.hpp>
+
+namespace mo {
+
+////////////////////////
+/// \brief 
+////////////////////////
+
+} // namespace mo
+#endif // VIDEO_H
