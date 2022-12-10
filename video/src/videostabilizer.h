1479674a7925ffca63899f78d2fa8a8fea0e7484
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2022-12-10 14:13:16 +0900

    implement videostabilizer library.

--- video/src/videostabilizer.h
+++ video/src/videostabilizer.h
@@ -1,11 +1,43 @@
 #ifndef VIDEOSTABILIZER_H
 #define VIDEOSTABILIZER_H
+#include <opencv2/core.hpp>
+#include <vector>
 
+namespace mo {
 
-class VideoStabilizer
-{
+class VideoStabilizer {
 public:
-    VideoStabilizer();
+  VideoStabilizer(const std::string &in_videoname);
+  void run();
+  struct Transform {
+    double dx;
+    double dy;
+    double da; // angle
+
+    void getTransform(cv::Mat &T) {
+      T.at<double>(0, 0) = cos(da);
+      T.at<double>(0, 1) = -sin(da);
+      T.at<double>(1, 0) = sin(da);
+      T.at<double>(1, 1) = cos(da);
+      T.at<double>(0, 2) = dx;
+      T.at<double>(1, 2) = dy;
+    }
+  };
+
+  struct Trajectory {
+    double x;
+    double y;
+    double a; // angle
+  };
+
+private:
+  std::vector<Trajectory> cumsum(std::vector<Transform> &transforms);
+  /// upscale k% fixing the center.
+  void enlargeImage(cv::Mat &frame_stabilized, float k = 1.05);
+
+  // variable
+  std::string in_videoname_;
 };
 
+} // namespace mo
 #endif // VIDEOSTABILIZER_H
