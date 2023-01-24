bcdc5ac8d67920e62d429703372304b0c7371f7f
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2023-01-24 16:00:43 +0900

    add sample_realsense_stabilizer

---
+++ mopf/video/sample/sample_realsense_stabilizer.cpp
@@ -0,0 +1,20 @@
+#include "mopf/video/src/video.hpp"
+#include "mopf/video/src/videostabilizer.hpp"
+#include "mopf/visualization/src/visualization.h"
+
+int main() {
+  mo::Image in_img;
+  mo::VideoCapture cap(cv::CAP_INTELPERC);
+  mo::VideoStabilizer vstab;
+
+  while (1) {
+    cap.grab();
+    cap.retrieve(in_img, cv::CAP_INTELPERC_IMAGE);
+
+    vstab.run(in_img);
+    vstab.show();
+  }
+
+  cap.release();
+  return 0;
+}
